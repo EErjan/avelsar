6030d02fde02df1baae027f4ba607003bf42f39c
eerjan <erjan.ergashev@mail.ru>
2021-03-30 22:16:25 +0500

    tab block

--- avelsar/js/main.js
+++ avelsar/js/main.js
@@ -90,3 +90,17 @@ try {
 }
 
 
+// TAB
+
+$(function(){
+    $('nav a').click(function(){
+        var tabId = $(this).attr('data-tab');
+
+        $('nav a').removeClass('active');
+        $('.Tabcondent').removeClass('active');
+
+        $(this).addClass('active');
+        $('#'+tabId).addClass('active');
+    });
+
+});
