5dd34e09ea368826b69b3ce34a666900aa6416f7
eerjan <erjan.ergashev@mail.ru>
2021-04-02 00:51:36 +0500

    preloader

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,9 @@
+setTimeout(function () {
+    $('.preloader').fadeOut();
+}, 1000);
 
-$(document).on("click", ".js-toggle", function(e) {
+
+$(document).on("click", ".js-toggle", function (e) {
     e.preventDefault();
     $("body").toggleClass("has-menu");
 });
@@ -8,18 +12,18 @@ $(document).on("click", ".js-toggle", function(e) {
 // Accordion start
 
 try {
-    $(function(){
-        var Accordion = function(el, multiple) {
+    $(function () {
+        var Accordion = function (el, multiple) {
             this.el = el || {};
             this.multiple = multiple || false;
 
             // Variables
             var link = this.el.find('.link');
             // Eventos
-            link.on('click', {el: this.el, multiple: this.multiple},this.dropdown)
+            link.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
         }
 
-        Accordion.prototype.dropdown = function(e) {
+        Accordion.prototype.dropdown = function (e) {
             var $el = e.data.el;
             $this = $(this),
                 $next = $this.next();
@@ -30,7 +34,7 @@ try {
             $this.parent().toggleClass('open');
 
             //Parametro inicial que permite ver 1 solo submenu abierto
-            if(!e.data.multiple){
+            if (!e.data.multiple) {
                 $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
             }
 
@@ -40,37 +44,37 @@ try {
 
     });
 
-    $(function(){
-    var Accordion = function(el, multiple) {
-      this.el = el || {};
-      this.multiple = multiple || false;
+    $(function () {
+        var Accordion = function (el, multiple) {
+            this.el = el || {};
+            this.multiple = multiple || false;
 
-      // Variables
-      var link = this.el.find('.link');
-      // Eventos
-      link.on('click', {el: this.el, multiple: this.multiple},this.dropdown)
-    }
+            // Variables
+            var link = this.el.find('.link');
+            // Eventos
+            link.on('click', {el: this.el, multiple: this.multiple}, this.dropdown)
+        }
 
-    Accordion.prototype.dropdown = function(e) {
-      var $el = e.data.el;
-      $this = $(this),
-          $next = $this.next();
+        Accordion.prototype.dropdown = function (e) {
+            var $el = e.data.el;
+            $this = $(this),
+                $next = $this.next();
 
-      // Desencadena evento de apertura en los elementos siguientes a la clase link = ul.submenu
-      $next.slideToggle();
-      // Agregar clase open a elemento padre del elemento con clase link = li
-      $this.parent().toggleClass('open');
+            // Desencadena evento de apertura en los elementos siguientes a la clase link = ul.submenu
+            $next.slideToggle();
+            // Agregar clase open a elemento padre del elemento con clase link = li
+            $this.parent().toggleClass('open');
 
-      //Parametro inicial que permite ver 1 solo submenu abierto
-      if(!e.data.multiple){
-        $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
-      }
+            //Parametro inicial que permite ver 1 solo submenu abierto
+            if (!e.data.multiple) {
+                $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+            }
 
-    }
-    // Elegir submenus multiples (true) submenus uno a la vez (false)
-    var accordion = new Accordion($('#accordion2'), false);
+        }
+        // Elegir submenus multiples (true) submenus uno a la vez (false)
+        var accordion = new Accordion($('#accordion2'), false);
 
-  });
+    });
 } catch (e) {
 
 }
@@ -78,7 +82,7 @@ try {
 // Accordion end
 
 
-$(".submenu__list-title").click(function(){
+$(".submenu__list-title").click(function () {
     $(".submenu__list-holder").toggleClass("hide");
 });
 
@@ -92,15 +96,15 @@ try {
 
 // TAB
 
-$(function(){
-    $('nav a').click(function(){
+$(function () {
+    $('nav a').click(function () {
         var tabId = $(this).attr('data-tab');
 
         $('nav a').removeClass('active');
         $('.Tabcondent').removeClass('active');
 
         $(this).addClass('active');
-        $('#'+tabId).addClass('active');
+        $('#' + tabId).addClass('active');
     });
 
 });
@@ -108,21 +112,23 @@ $(function(){
 
 // Butto effect
 
-$(function() {
-  $('.btn-6')
-      .on('mouseenter', function(e) {
-        var parentOffset = $(this).offset(),
-            relX = e.pageX - parentOffset.left,
-            relY = e.pageY - parentOffset.top;
-        $(this).find('span').css({top:relY, left:relX})
-      })
-      .on('mouseout', function(e) {
-        var parentOffset = $(this).offset(),
-            relX = e.pageX - parentOffset.left,
-            relY = e.pageY - parentOffset.top;
-        $(this).find('span').css({top:relY, left:relX})
-      });
-  $('[href=#]').click(function(){return false});
+$(function () {
+    $('.btn-6')
+        .on('mouseenter', function (e) {
+            var parentOffset = $(this).offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+            $(this).find('span').css({top: relY, left: relX})
+        })
+        .on('mouseout', function (e) {
+            var parentOffset = $(this).offset(),
+                relX = e.pageX - parentOffset.left,
+                relY = e.pageY - parentOffset.top;
+            $(this).find('span').css({top: relY, left: relX})
+        });
+    $('[href=#]').click(function () {
+        return false
+    });
 });
 
 // AOS
